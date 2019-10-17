999a122fe01bc58b6503a4a0e3325332db9f6ec7
Anton Hörnquist <anton.hornquist@gmail.com>
2019-10-17 23:46:13 +0200

    refactor

--- bob.lua
+++ bob.lua
@@ -6,8 +6,8 @@ engine.name = 'R'
 local Formatters = require('formatters')
 local R = require('r/lib/r') -- assumes r engine resides in ~/dust/code/r folder
 local UI = include('lib/ui')
+local Pages = include('lib/pages')
 
-local current_page = 1
 local fps = 120
 
 local function create_modules()
@@ -93,16 +93,66 @@ local function init_params()
 end
 
 local function refresh_ui()
-  if target_page then
-    current_page = current_page + page_trans_div
-    page_trans_frames = page_trans_frames - 1
-    if page_trans_frames == 0 then
-      current_page = target_page
-      target_page = nil
-    end
-    UI.set_dirty()
-  end
+  Pages.refresh(UI)
   UI.refresh()
+end
+
+local function init_pages()
+  local function format_percentage(value)
+    return util.round(value*100, 1) .. "%"
+  end
+
+  local function format_freq(hz)
+    if hz < 1 then
+      local str = tostring(util.round(hz, 0.001))
+      return string.sub(str, 2, #str).."Hz"
+    elseif hz < 10 then
+      return util.round(hz, 0.01).."Hz"
+    elseif hz < 100 then
+      return util.round(hz, 0.1).."Hz"
+    elseif hz < 1000 then
+      return util.round(hz, 1).."Hz"
+    elseif hz < 10000 then
+      return util.round(hz/1000, 0.1) .. "kHz"
+    else
+      return util.round(hz/1000, 1) .. "kHz"
+    end
+  end
+
+  local ui_params = {
+    {
+      {
+        label="FREQ",
+        id="cutoff",
+        value=function(id)
+          return format_freq(params:get(id))
+        end
+      },
+      {
+        label="RES",
+        id="resonance",
+        value=function(id)
+          return format_percentage(params:get(id))
+        end
+      }
+    },
+    {
+      {
+        label="LFO",
+        id="lfo_rate",
+        value=function(id)
+          return format_freq(params:get(id))
+        end
+      },
+      {
+        label="L>FRQ",
+        id="lfo_to_cutoff",
+        value=function(id)
+          return format_percentage(params:get(id))
+        end
+      }
+    }
+  }
 end
 
 local function init_ui_refresh_metro()
@@ -143,8 +193,10 @@ end
 function init()
   create_modules()
   connect_modules()
+
   init_params()
   init_ui()
+  init_pages()
 
   params:read()
   params:bang()
@@ -154,200 +206,16 @@ function cleanup()
   params:write()
 end
 
-local function format_percentage(value)
-  return util.round(value*100, 1) .. "%"
-end
-
-local function format_freq(hz)
-  if hz < 1 then
-    local str = tostring(util.round(hz, 0.001))
-    return string.sub(str, 2, #str).."Hz"
-  elseif hz < 10 then
-    return util.round(hz, 0.01).."Hz"
-  elseif hz < 100 then
-    return util.round(hz, 0.1).."Hz"
-  elseif hz < 1000 then
-    return util.round(hz, 1).."Hz"
-  elseif hz < 10000 then
-    return util.round(hz/1000, 0.1) .. "kHz"
-  else
-    return util.round(hz/1000, 1) .. "kHz"
-  end
-end
-
-local ui_params = {
-  {
-    {
-      label="FREQ",
-      id="cutoff",
-      value=function(id)
-        return format_freq(params:get(id))
-      end
-    },
-    {
-      label="RES",
-      id="resonance",
-      value=function(id)
-        return format_percentage(params:get(id))
-      end
-    }
-  },
-  {
-    {
-      label="LFO",
-      id="lfo_rate",
-      value=function(id)
-        return format_freq(params:get(id))
-      end
-    },
-    {
-      label="L>FRQ",
-      id="lfo_to_cutoff",
-      value=function(id)
-        return format_percentage(params:get(id))
-      end
-    }
-  }
-}
-
-local num_pages = #ui_params
-
 function redraw()
-  local hi_level = 15
-  local lo_level = 4
-
-  local enc1_x = 0
-  local enc1_y = 12
-
-  local enc2_x = 10
-  local enc2_y = 33
-
-  local enc3_x = enc2_x+65
-  local enc3_y = enc2_y
-
-  local key2_x = 0
-  local key2_y = 63
-
-  local key3_x = key2_x+65
-  local key3_y = key2_y
-
-  local function redraw_enc1_widget()
-    screen.move(enc1_x, enc1_y)
-
-    screen.level(lo_level)
-    screen.text("LEVEL")
-    screen.move(enc1_x+45, enc1_y)
-    screen.level(hi_level)
-    screen.text(util.round(mix:get_raw("output")*100, 1))
-  end
-
-  local function redraw_event_flash_widget()
-    screen.level(lo_level)
-    screen.rect(122, enc1_y-7, 5, 5)
-    screen.fill()
-  end
-
-  local function draw_ui_param(page, param_index, x, y)
-    local ui_param = ui_params[page][param_index]
-    screen.move(x, y)
-    screen.level(lo_level)
-    screen.text(ui_param.label)
-    screen.move(x, y+12)
-    screen.level(hi_level)
-    screen.text(ui_param.value(ui_param.id))
-  end
-
-  local function redraw_enc2_widget()
-    local left = math.floor(current_page)
-    local right = math.ceil(current_page)
-    local offset = current_page - left
-    local pixel_ofs = util.round(offset*128)
-
-    draw_ui_param(left, 1, enc2_x-pixel_ofs, enc2_y)
-
-    if left ~= right then
-      draw_ui_param(right, 1, enc2_x+128-pixel_ofs, enc2_y)
-    end
-  end
-
-  local function redraw_enc3_widget()
-    local left = math.floor(current_page)
-    local right = math.ceil(current_page)
-    local offset = current_page - left
-    local pixel_ofs = util.round(offset*128)
-
-    draw_ui_param(left, 2, enc3_x-pixel_ofs, enc3_y)
-
-    if left ~= right then
-      draw_ui_param(right, 2, enc3_x+128-pixel_ofs, enc3_y)
-    end
-  end
-    
-  local function redraw_page_indicator()
-    local div = 128/num_pages
-    screen.level(lo_level)
-    screen.rect(util.round((current_page-1)*div), enc2_y+15, util.round(div), 2)
-    screen.fill()
-  end
-
-  local function redraw_key2_widget()
-    screen.move(key2_x, key2_y)
-    if prev_held then
-      screen.level(hi_level)
-    else
-      screen.level(lo_level)
-    end
-    screen.text("PREV")
-  end
-
-  local function redraw_key3_widget()
-    screen.move(key3_x, key3_y)
-    if next_held then
-      screen.level(hi_level)
-    else
-      screen.level(lo_level)
-    end
-    screen.text("NEXT")
-  end
-
-  screen.font_size(16)
-  screen.clear()
-
-  redraw_enc1_widget()
-
-  if UI.show_event_indicator then
-    redraw_event_flash_widget()
-  end
-
-  redraw_enc2_widget()
-  redraw_enc3_widget()
-
-  redraw_page_indicator()
-
-  redraw_key2_widget()
-  redraw_key3_widget()
-
-  screen.update()
-end
-
-function get_current_page_param_id(n)
-  local page = util.round(current_page)
-  return ui_params[page][n].id
-end
-
-function transition_to_page(page)
-  source_page = current_page
-  target_page = page
-  page_trans_frames = fps/5
-  page_trans_div = (target_page - source_page) / page_trans_frames
+  Pages.redraw(screen, UI)
 end
 
 function change_current_page_param_delta(n, delta)
-  params:delta(get_current_page_param_id(n), delta)
+  params:delta(Pages.get_current_page_param_id(n), delta)
 end
 
 function change_current_page_param_raw_delta(n, rawdelta)
-  local id = get_current_page_param_id(n)
+  local id = Pages.get_current_page_param_id(n)
   local val = params:get_raw(id)
   params:set_raw(id, val+rawdelta)
 end
@@ -368,37 +236,5 @@ function enc(n, delta)
 end
 
 function key(n, z)
-  local page
-  if target_page then
-    page = target_page
-  else
-    page = util.round(current_page)
-  end
-  if n == 2 then
-    if z == 1 then
-      page = page - 1
-      if page < 1 then
-        page = num_pages
-      end
-      transition_to_page(page)
-
-      prev_held = true
-    else
-      prev_held = false
-    end
-    UI.set_dirty()
-  elseif n == 3 then
-    if z == 1 then
-      page = page + 1
-      if page > num_pages then
-        page = 1
-      end
-      transition_to_page(page)
-
-      next_held = true
-    else
-      next_held = false
-    end
-    UI.set_dirty()
-  end
+  Pages.key(n, z, UI)
 end
