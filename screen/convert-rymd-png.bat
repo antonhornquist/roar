45135db6f430ca3849d5639003d44655d7357edd
Anton Hörnquist <anton.hornquist@gmail.com>
2020-02-29 09:54:14 +0100

    add screenshots

---
+++ screen/convert-rymd-png.bat
@@ -0,0 +1,1 @@
+magick convert "rymd-dump.png" -gamma "1.25" -filter "point" -resize "300%%" -gravity "center" -background "black" -extent "110%%" "rymd.png"
---
+++ screen/convert-skev-png.bat
@@ -0,0 +1,1 @@
+magick convert "skev-dump-2.png" -gamma "1.25" -filter "point" -resize "300%%" -gravity "center" -background "black" -extent "110%%" "skev.png"
